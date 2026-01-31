c4280d39372e62af5152b37acf32249722666313
parvinbiti <parvinbiti365@gmail.com>
2026-01-31 00:00:25 -0300

    4

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'NoorMart'
+export const APP_SLOGAN =
+    process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more.'
+export const APP_DESCRIPTION =
+    process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+    'NoorMart is a modern, fast, and secure e-commerce platform inspired by Amazon. Built with Next.js and MongoDB, NoorMart delivers a seamless shopping experience with lightning-fast performance, smart search, secure payments, and scalable architecture—designed for the future of online shopping.'
