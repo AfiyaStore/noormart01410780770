7e9fcb360cd1137843542339fad1dca6f8068f7f
parvinbiti <parvinbiti365@gmail.com>
2026-02-02 14:14:21 -0300

    13-1

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { APP_DESCRIPTION, APP_NAME, APP_SLOGAN } from '@/lib/constants'
 import { Toaster } from '@/components/ui/sonner' // ✅ ADD THIS
+import ClientProviders from '@/components/shared/client-providers'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -73,7 +74,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClientProviders>{children}</ClientProviders>
 
         {/* ✅ SONNER TOASTER */}
         <Toaster richColors position="top-right" />
