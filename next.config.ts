3a0bccdbdb69af0ce4818194eebe75d4c1e94c85
parvinbiti <parvinbiti365@gmail.com>
2026-02-08 14:41:06 -0300

    30-admin-orders

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,40 @@
 import type { NextConfig } from "next";
 
+/* config options here */
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: '',
+      },
+    ],
+  },
+}
 export default nextConfig;
+
+// import type { NextConfig } from 'next'
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'utfs.io',
+//         port: '',
+//       },
+//     ],
+//   },
+
+//   // Webpack customization to handle .md files (Uploadthing dependency)
+//   webpack: (config) => {
+//     config.module.rules.push({
+//       test: /\.md$/,
+//       type: 'asset/source', // treat README.md as plain source
+//     })
+//     return config
+//   },
+// }
+
+// export default nextConfig
